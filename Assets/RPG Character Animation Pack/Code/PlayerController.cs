926b2c549f901e7be0b95282aa25ae374ce67fad
Blueeyer27 <anthonyla2@gmail.com>
2020-03-03 03:47:48 +0200

    Added bloody trails with particles to One Hand Axe, turned off previous effects

--- Assets/RPG Character Animation Pack/Code/PlayerController.cs
+++ Assets/RPG Character Animation Pack/Code/PlayerController.cs
@@ -963,7 +963,7 @@ public class PlayerController : MonoBehaviour{
 						//Right sword has 6 attacks
 						else if(rightWeapon == 9){
 							attackNumber = Random.Range(7, 12);
-                            animationEffects.InstantiateEffect(attackNumber - 7);
+                            //animationEffects.InstantiateEffect(attackNumber - 7);
                         }
 						else{
 							attackNumber = Random.Range(3, maxAttacks + 3);
