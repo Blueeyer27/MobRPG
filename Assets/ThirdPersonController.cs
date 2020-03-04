a8b8acdd5809214d1102f063f97e14c8850820b3
Blueeyer27 <anthonyla2@gmail.com>
2020-03-04 02:35:52 +0200

    Removed shadows, changed lights, added lowpoly skybox

--- Assets/ThirdPersonController.cs
+++ Assets/ThirdPersonController.cs
@@ -34,7 +34,7 @@ public class ThirdPersonController : MonoBehaviour
 
         CameraAngle += touchField.TouchDist.x * CameraAngleSpeed;
 
-        Camera.main.transform.position = transform.position + Quaternion.AngleAxis(CameraAngle, Vector3.up) * new Vector3(0, 6, 8);
+        Camera.main.transform.position = transform.position + Quaternion.AngleAxis(CameraAngle, Vector3.up) * new Vector3(0, 6, -8);
         Camera.main.transform.rotation = Quaternion.LookRotation(transform.position + Vector3.up * 2f - Camera.main.transform.position, Vector3.up);
     }
 
