98be87adf74380643d825643ca0aae1db0c79064
Blueeyer27 <anthonyla2@gmail.com>
2020-03-11 23:11:46 +0200

    Created new Animation controller for Archer, applied some logic to Archer class

---
+++ Assets/AnimationEvents.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEvents : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //Placeholder functions for Animation events
+    public void Hit()
+    {
+    }
+
+    public void Shoot()
+    {
+    }
+
+    public void FootR()
+    {
+    }
+
+    public void FootL()
+    {
+    }
+
+    public void Land()
+    {
+    }
+
+    public void WeaponSwitch()
+    {
+    }
+}
